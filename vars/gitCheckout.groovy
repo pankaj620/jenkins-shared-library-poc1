835021baf1bdcc23f665c0e925778ae7e36a4230
pankaj620 <59977514+pankaj620@users.noreply.github.com>
2025-04-12 13:28:58 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,5 +1,6 @@
 def call(String repoUrl, String branch){
 	def workingDir = "${env.WORKSPACE}"
+	sh "rm -rf ${env.WORKSPACE}"
 	sh "git clone ${repoUrl} ${workingDir}"
         sh "git checkout ${branch}"
 	return workingDir
